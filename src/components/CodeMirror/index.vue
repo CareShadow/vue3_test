66226d72385d02639b0259d29a502c0a83e7fd28
careshadow <1845161518@qq.com>
2024-01-05 18:23:26 +0800

    组件改造

--- src/components/CodeMirror/index.vue
+++ src/components/CodeMirror/index.vue
@@ -1,11 +1,10 @@
 <template>
-    <codemirror v-model="code" placeholder="Please start your inquiry" :style="{ height: '400px' }" :autofocus="true"
-        :indent-with-tab="true" :tab-size="2" :extensions="extensions" @ready="handleReady" @change="log('change', $event)"
-        @focus="log('focus', $event)" @blur="log('blur', $event)" />
+    <codemirror v-model="code" placeholder="Please start your inquiry" :style="{ height: '400px', fontSize: '16px' }"
+        :autofocus="true" :indent-with-tab="true" :tab-size="2" :extensions="extensions" />
 </template>
   
-<script>
-import { ref, shallowRef } from 'vue'
+<script setup>
+import { ref } from 'vue'
 import { Codemirror } from 'vue-codemirror'
 import { sql } from '@codemirror/lang-sql'
 import { basicSetup } from 'codemirror'
@@ -13,40 +12,12 @@ import { tags } from "@lezer/highlight"
 import { HighlightStyle, syntaxHighlighting } from "@codemirror/language"
 import { oneDark } from '@codemirror/theme-one-dark'
 
-export default {
-    name: 'CodeEditor',
-    components: {
-        Codemirror
-    },
-    setup() {
-        const code = ref('select * from table')
+const code = ref('select * from table')
 
-        const myHighlightStyle = HighlightStyle.define([
-            { tag: tags.keyword, color: "navy", fontWeight: "700", lineHeight: "1em" },
-            { tag: tags.comment, color: "green", fontStyle: "italic" }
-        ])
-        const extensions = [sql(), oneDark, basicSetup, syntaxHighlighting(myHighlightStyle)]
+const myHighlightStyle = HighlightStyle.define([
+    { tag: tags.keyword, color: "navy", fontWeight: "700", lineHeight: "1em" },
+    { tag: tags.comment, color: "green", fontStyle: "italic" }
+])
+const extensions = [sql(), oneDark, basicSetup, syntaxHighlighting(myHighlightStyle)]
 
-        const view = shallowRef()
-        const handleReady = (payload) => {
-            view.value = payload.view
-        }
-
-        const getCodemirrorStates = () => {
-            const state = view.value.state
-            const ranges = state.selection.ranges
-            const selected = ranges.reduce((r, range) => r + range.to - range.from, 0)
-            const cursor = ranges[0].anchor
-            const length = state.doc.length
-            const lines = state.doc.lines
-        }
-
-        return {
-            code,
-            extensions,
-            handleReady,
-            log: console.log
-        }
-    }
-}
 </script>
--- src/components/index.js
+++ src/components/index.js
@@ -3,17 +3,15 @@ import SvgIcon from './SvgIcon/index.vue';
 import * as ELementPlusIconsVue from '@element-plus/icons-vue'
 import VueDragResize from 'vue-drag-resize'
 import CodeEditor from './CodeMirror/index.vue'
-const allGloablComponent = { SvgIcon, CodeEditor };
+const allGloablComponent = { SvgIcon, CodeEditor, VueDragResize };
 // 自定义插件
 export default {
     install(app) {
         Object.keys(allGloablComponent).forEach(key => {
-            app.component(key);
+            app.component(key, allGloablComponent[key]);
         });
         for (const [key, component] of Object.entries(ELementPlusIconsVue)) {
             app.component(key, component)
         }
-        app.component('VueDragResize', VueDragResize);
-        app.component('CodeEditor', CodeEditor);
     }
 }

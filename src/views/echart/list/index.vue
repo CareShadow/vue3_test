79db2924e1707b600892cb02cb60ec1ded290dc4
careshadow <1845161518@qq.com>
2024-03-17 20:58:28 +0800

    多个图表宽高的调整，待解决：选择框输入会重置组件的状态

--- src/views/echart/list/index.vue
+++ src/views/echart/list/index.vue
@@ -22,26 +22,31 @@
       <div class="draw">
         <div class="tools_box">
           <label for="box_width">W:</label>
-          <input id="box_width" type="text" class="mi-input" v-model="selectedList[0].width">
+          <input id="box_width" type="text" class="mi-input" autocomplete="off" v-model.number.lazy="showWidth">
           <label for="box_height">H:</label>
-          <input id="box_height" type="text" class="mi-input" v-model="selectedList[0].height">
+          <input id="box_height" type="text" class="mi-input" autocomplete="off" v-model.number.lazy="showHeight">
           <label for="box_left">X:</label>
-          <input id="box_left" type="text" class="mi-input" v-model="selectedList[0].left">
+          <input id="box_left" type="text" class="mi-input" autocomplete="off" v-model.number.lazy="showLeft">
           <label for="box_top">Y:</label>
-          <input id="box_top" type="text" class="mi-input" v-model="selectedList[0].top">
+          <input id="box_top" type="text" class="mi-input" autocomplete="off" v-model.number.lazy="showTop">
           <label for="box_zindex">Z:</label>
-          <input id="box_zindex" type="text" class="mi-input" v-model="selectedList[0].zindex">
+          <input id="box_zindex" type="text" class="mi-input" autocomplete="off" v-model.number.lazy="showZindex">
         </div>
         <div ref="drawArea" @dragover.prevent @drop="createEcharts($event)" class="drawArea">
           <VueDragResize v-for="item in echartList"
                          :is-resizable="true"
+                         :is-active="item.isActive"
                          :w="parseInt(item.width)"
                          :h="parseInt(item.height)"
+                         :minw="0"
+                         :minh="0"
                          :x="parseInt(item.left)"
                          :y="parseInt(item.top)"
+                         :z="item.zIndex"
                          :parent-limitation="true"
                          @resizing="resize($event, item)"
                          @activated="onActivated(item)"
+                         @deactivated="onDeactivated(item)"
                          @dragging="resize($event, item)">
             <div :style="item" :id="item.id">
             </div>
@@ -54,28 +59,71 @@
 
 <script setup>
 
-import {ref, nextTick} from "vue";
+import {ref, nextTick, computed, watch} from "vue";
 import {v4 as uuidv4} from "uuid";
 import echarts from '@/hooks/echartHook';
 
 // 被选择元素的数组
-const selectedList = ref([{}]);
+const selectedList = ref([]);
 // 变量必须和ref="drawArea"同名
 const drawArea = ref(null);
 // 控件列表变量
 const echartList = ref([]);
+// 显示框信息
+const showWidth = computed({
+  get() {
+    return selectedList.value.length > 0 ? parseInt(selectedList.value[0].width) : null;
+  },
+  set(newValue) {
+    selectedList.value[0].width = newValue + 'px'
+  }
+});
+const showHeight = computed({
+  get() {
+    return selectedList.value.length > 0 ? parseInt(selectedList.value[0].height) : null;
+  },
+  set(newValue) {
+    selectedList.value[0].height = newValue + 'px'
+  }
+});
+const showLeft = computed({
+  get() {
+    return selectedList.value.length > 0 ? parseInt(selectedList.value[0].left) : null;
+  },
+  set(newValue) {
+    selectedList.value[0].left = newValue + 'px'
+  }
+});
+const showTop = computed({
+  get() {
+    return selectedList.value.length > 0 ? parseInt(selectedList.value[0].top) : null;
+  },
+  set(newValue) {
+    selectedList.value[0].top = newValue + 'px'
+  }
+});
+const showZindex = computed({
+  get() {
+    return selectedList.value.length > 0 ? parseInt(selectedList.value[0].zIndex) : null;
+  },
+  set(newValue) {
+    selectedList.value[0].zIndex = newValue
+  }
+});
+
 // 拖拽创建组件,向列表中添加对象
 const createEcharts = (e) => {
   const uuid = uuidv4();
   const item = {
     id: uuid,
+    isActive: true,
     width: 200 + "px",
     height: 150 + "px",
     backgroundColor: "#90EE90",
     opacity: 0.3,
     left: e.clientX - 310 + "px",
     top: e.clientY + "px",
-    zindex: 0
+    zIndex: 0
   }
   echartList.value.push(item);
   nextTick(() => {
@@ -118,9 +166,17 @@ const resize = (e, item) => {
 }
 
 const onActivated = (item) => {
-  selectedList.value.pop();
+  // 点击元素将它的配置选项数值放置到被选择的元素数组上
+  item.isActive = true;
   selectedList.value.push(item);
 }
+
+const onDeactivated = (item) => {
+  const index = selectedList.value.findIndex(obj => obj.id = item.id);
+  selectedList.value.splice(index, 1);
+  item.isActive = false;
+}
+
 </script>
 
 <style lang="scss" scoped>

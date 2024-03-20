b29da5d780e3cc90ae9e9bdad55bab36d0bcf5ab
careshadow <1845161518@qq.com>
2024-03-20 22:00:34 +0800

    将Vue-Drag-Resize包加入项目, 方便重写其中不足的逻辑

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 // 引入项目中全部的全局组件
 import SvgIcon from './SvgIcon/index.vue';
 import * as ELementPlusIconsVue from '@element-plus/icons-vue'
-import VueDragResize from 'vue-drag-resize'
+import VueDragResize from './VueDragResize/index.vue'
 import CodeEditor from './CodeMirror/index.vue'
 const allGloablComponent = { SvgIcon, CodeEditor, VueDragResize };
 // 自定义插件
